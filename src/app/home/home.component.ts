c9e06522e2586390e5c423197eaca7a12467723f
VictorAlencar00 <victordesousaa@gmail.com>
2024-04-21 19:24:46 -0300

    feat: services add

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,8 +19,11 @@ import { HousingLocation } from '../housinglocation';
       </form>
     </section>
     <section class="results">
-      <app-housing-location></app-housing-location>
-    </section>
+    <app-housing-location
+      *ngFor="let housingLocation of housingLocationList"
+      [housingLocation]="housingLocation">
+    </app-housing-location>
+  </section>
   `,
   styleUrls: ['./home.component.css'],
 })
@@ -28,14 +31,10 @@ import { HousingLocation } from '../housinglocation';
 export class HomeComponent {
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
-  housingLocation: HousingLocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 }
